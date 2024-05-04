719affad52d83e1c5421c159b691f816f5a17b1a
lukman <lukman.fyrtio@gmail.com>
2024-05-05 02:15:17 +0700

    Add version api on open api docs

--- src/main/java/com/swamedia/sistempengaduankeluhan/OpenApiConfig.java
+++ src/main/java/com/swamedia/sistempengaduankeluhan/OpenApiConfig.java
@@ -16,6 +16,6 @@ public class OpenApiConfig {
 		return new OpenAPI().components(new Components())
 				.info(new Info().title("Sistem Aplikasi Pengaduan Warga").description("\r\n"
 						+ "The \"Sistem Aplikasi Pengaduan Warga\" (Citizen Complaint Management System) is a web-based application designed to facilitate the process of reporting and managing complaints from citizens. It serves as a centralized platform for citizens to submit their complaints regarding various issues, such as public services, infrastructure problems, or community concerns.")
-						.contact(new Contact().name("Lukman").url("https://github.com/lukmanfyrtio")));
+						.contact(new Contact().name("Lukman").url("https://github.com/lukmanfyrtio")).version("1.0"));
 	}
 }
